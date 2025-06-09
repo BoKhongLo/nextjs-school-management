5d79430f2c4c1376e09a330c3813a1da4299d876
nguoidepxuhue <ngocungducanh2022@gmail.com>
2025-06-09 14:24:59 +0700

    add financeChart

--- frontend/src/app/(dashboard)/admin/page.tsx
+++ frontend/src/app/(dashboard)/admin/page.tsx
@@ -1,4 +1,7 @@
+
+import AttendanceChart from "@/components/AttendanceChart";
 import CountChart from "@/components/CountChart";
+import FinanceChart from "@/components/FinanceChart";
 import UserCard from "@/components/UserCard";
 
 const AdminPage = () => {
@@ -19,12 +22,15 @@ const AdminPage = () => {
             <CountChart />
           </div>
           {/* attendance chart */}
-          <div className=" w-full lg:w-2/3  h-[450px]"></div>
+          <div className=" w-full lg:w-2/3  h-[450px]">
+            <AttendanceChart />
+          </div>
         </div>
-
-        
+        {/* bottomchart */}
+          <div className="w-full h-[500px]">
+            <FinanceChart/>
+          </div>
       </div>
-
       {/* right */}
       <div className="w-full lg:w-1/3">r</div>
     </div>
